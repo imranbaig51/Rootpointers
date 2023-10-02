9399815aed6e3677622f532b55b12477f5948f28
imra <ibaig5147@gmail.com>
2023-10-02 16:21:52 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html",],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#000",
+        "secondary": "#272D35"
+      }
+    },
+  },
+  plugins: [],
+}
+
