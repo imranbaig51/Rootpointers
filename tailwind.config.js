0809146ecaea0e437505b8ac5ac443eaedc3bf24
imra <ibaig5147@gmail.com>
2023-10-05 18:38:21 +0500

    Today's Work

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       colors: {
         "primary": "#000",
-        "secondary": "#272D35"
+        "secondary": "#4A83E7"
       },
       fontSize: {
         sm: ['14px'],
@@ -16,21 +16,27 @@ module.exports = {
         header: ['36px'],
       },
     },
+    // container: {
+    //   sm: ['540px'],
+    //   md: ['700px'],
+    //   lg: ['900px'],
+    //   xl: ['1140px'],
+    // },
     screens: {
       'sm': '576px',
-      // => @media (min-width: 640px) { ... }
+      // => @media (min-width: 5765px) { ... }
 
       'md': '768px',
       // => @media (min-width: 768px) { ... }
 
       'lg': '992px',
-      // => @media (min-width: 1024px) { ... }
+      // => @media (min-width: 992px) { ... }
 
       'xl': '1200px',
-      // => @media (min-width: 1280px) { ... }
+      // => @media (min-width: 1200px) { ... }
 
       '2xl': '1400px',
-      // => @media (min-width: 1536px) { ... }
+      // => @media (min-width: 1400px) { ... }
     }
   },
   plugins: [],
